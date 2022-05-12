9ca7de2a56ae7f662df2980c9f5bcd3d587889bb
itosatone <satone.ito@rakus-partners.co.jp>
2022-05-12 12:05:30 +0900

    exam02 commit

--- ex-bigginer/src/main/java/com/example/domain/Member.java
+++ ex-bigginer/src/main/java/com/example/domain/Member.java
@@ -2,6 +2,24 @@ package com.example.domain;
 
 public class Member {
 	private String name;
+	private int num1;
+	private int num2;
+
+	public int getNum1() {
+		return num1;
+	}
+
+	public void setNum1(int num1) {
+		this.num1 = num1;
+	}
+
+	public int getNum2() {
+		return num2;
+	}
+
+	public void setNum2(int num2) {
+		this.num2 = num2;
+	}
 
 	public String getName() {
 		return name;
